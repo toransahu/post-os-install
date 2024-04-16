0330c45e782c50c1a41b4c445c2c0fa8ebee2778
Toran Sahu <toran.sahu@yahoo.com>
2024-04-16 20:56:23 +0530

    shfmt

--- setup.sh
+++ setup.sh
@@ -17,175 +17,160 @@ DOTFILES_PVT=".2fa .gitconfig .gitconfig-aristanetworks"
 DOTDIRS=".personalized "
 DOTDIRS_PVT=".config .ssh "
 
-
 setup_workspace_dir() {
-	mkdir -p $GH_DIR
+    mkdir -p $GH_DIR
 }
 
 clone_repos() {
-	for repo in ${REPOS[@]}
-	do
-		if  [ ! -d "$GH_DIR/$GH_ME/$repo" ]; then
-			git clone https://github.com/toransahu/$repo $GH_DIR/$GH_ME/$repo
-		else
-			echo $repo already cloned. Pulling updates
-			cd $GH_DIR/$GH_ME/$repo	&& git pull origin master && cd -
-		fi
-	done
+    for repo in ${REPOS[@]}; do
+        if [ ! -d "$GH_DIR/$GH_ME/$repo" ]; then
+            git clone https://github.com/toransahu/$repo $GH_DIR/$GH_ME/$repo
+        else
+            echo $repo already cloned. Pulling updates
+            cd $GH_DIR/$GH_ME/$repo && git pull origin master && cd -
+        fi
+    done
 }
 
-
 copy_dotfiles() {
-	SOURCE=$1
-	shift
-	DEST=$1
-	shift
-	echo Running copy_dotfiles $SOURCE $DEST
-	for file in $@
-	do
-		cp "$SOURCE/$file" "$DEST/"
-	done
+    SOURCE=$1
+    shift
+    DEST=$1
+    shift
+    echo Running copy_dotfiles $SOURCE $DEST
+    for file in $@; do
+        cp "$SOURCE/$file" "$DEST/"
+    done
 }
 
 copy_dotdirs() {
-	SOURCE=$1
-	shift
-	DEST=$1
-	shift
-	echo Running copy_dotdirs $SOURCE $DEST
-	for dir in $@
-	do
-		cp -r $SOURCE/$dir $DEST/
-	done
+    SOURCE=$1
+    shift
+    DEST=$1
+    shift
+    echo Running copy_dotdirs $SOURCE $DEST
+    for dir in $@; do
+        cp -r $SOURCE/$dir $DEST/
+    done
 }
 
 deploy_dotfiles() {
-	BACKUP_DIR=~/dotfiles-backup/$(date +%y%m%dT%H%M%S)
-	mkdir -p $BACKUP_DIR
-	copy_dotfiles $HOME $BACKUP_DIR/ $DOTFILES
-	copy_dotfiles $GH_DIR/$GH_ME/dotfiles ~/ $DOTFILES
+    BACKUP_DIR=~/dotfiles-backup/$(date +%y%m%dT%H%M%S)
+    mkdir -p $BACKUP_DIR
+    copy_dotfiles $HOME $BACKUP_DIR/ $DOTFILES
+    copy_dotfiles $GH_DIR/$GH_ME/dotfiles ~/ $DOTFILES
 }
 
 deploy_dotfiles_pvt() {
-	BACKUP_DIR=~/dotfiles-backup/$(date +%y%m%dT%H%M%S)
-	mkdir -p $BACKUP_DIR
-	copy_dotfiles $HOME $BACKUP_DIR/ $DOTFILES_PVT
-	copy_dotfiles $GH_DIR/$GH_ME/dotfiles.pvt ~/ $DOTFILES_PVT
+    BACKUP_DIR=~/dotfiles-backup/$(date +%y%m%dT%H%M%S)
+    mkdir -p $BACKUP_DIR
+    copy_dotfiles $HOME $BACKUP_DIR/ $DOTFILES_PVT
+    copy_dotfiles $GH_DIR/$GH_ME/dotfiles.pvt ~/ $DOTFILES_PVT
 }
 
 deploy_dotdirs() {
-	BACKUP_DIR=~/dotfiles-backup/$(date +%y%m%dT%H%M%S)
-	mkdir -p $BACKUP_DIR
-	copy_dotdirs $HOME $BACKUP_DIR/ $DOTDIRS
-	copy_dotdirs $GH_DIR/$GH_ME/dotfiles ~/ $DOTDIRS
+    BACKUP_DIR=~/dotfiles-backup/$(date +%y%m%dT%H%M%S)
+    mkdir -p $BACKUP_DIR
+    copy_dotdirs $HOME $BACKUP_DIR/ $DOTDIRS
+    copy_dotdirs $GH_DIR/$GH_ME/dotfiles ~/ $DOTDIRS
 }
 
 deploy_dotdirs_pvt() {
-	dirs=".config .ssh "
-	BACKUP_DIR=~/dotfiles-backup/$(date +%y%m%dT%H%M%S)
-	mkdir -p $BACKUP_DIR
-	copy_dotdirs $HOME $BACKUP_DIR/ $DOTDIRS_PVT
-	copy_dotdirs $GH_DIR/$GH_ME/dotfiles.pvt ~/ $DOTDIRS_PVT
+    dirs=".config .ssh "
+    BACKUP_DIR=~/dotfiles-backup/$(date +%y%m%dT%H%M%S)
+    mkdir -p $BACKUP_DIR
+    copy_dotdirs $HOME $BACKUP_DIR/ $DOTDIRS_PVT
+    copy_dotdirs $GH_DIR/$GH_ME/dotfiles.pvt ~/ $DOTDIRS_PVT
 }
 
 vcs_dotfiles() {
-	copy_dotfiles $HOME $GH_DIR/$GH_ME/dotfiles $DOTFILES
-	copy_dotfiles $HOME $GH_DIR/$GH_ME/dotfiles.pvt $DOTFILES_PVT
-	copy_dotdirs $HOME $GH_DIR/$GH_ME/dotfiles $DOTDIRS
-	copy_dotdirs $HOME $GH_DIR/$GH_ME/dotfiles.pvt $DOTDIRS_PVT
+    copy_dotfiles $HOME $GH_DIR/$GH_ME/dotfiles $DOTFILES
+    copy_dotfiles $HOME $GH_DIR/$GH_ME/dotfiles.pvt $DOTFILES_PVT
+    copy_dotdirs $HOME $GH_DIR/$GH_ME/dotfiles $DOTDIRS
+    copy_dotdirs $HOME $GH_DIR/$GH_ME/dotfiles.pvt $DOTDIRS_PVT
 }
-
 
 install_additional_packages() {
-	wget -O - https://raw.githubusercontent.com/toransahu/post-linux-install/master/src/setup_additional_pkg.sh | sh
+    wget -O - https://raw.githubusercontent.com/toransahu/post-linux-install/master/src/setup_additional_pkg.sh | sh
 }
 
-
 setup_python() {
-	items="pyenv_prereq pyenv"
-	for i in $items
-	do
-  		source $GH_DIR/$GH_ME/post-$X_PLATFORM-install/src/install_$i.sh
-	done
+    items="pyenv_prereq pyenv"
+    for i in $items; do
+        source $GH_DIR/$GH_ME/post-$X_PLATFORM-install/src/install_$i.sh
+    done
 }
 
 setup_java() {
-	items="jenv "
-	for i in $items
-	do
-  		source $GH_DIR/$GH_ME/post-$X_PLATFORM-install/src/install_$i.sh
-	done
+    items="jenv "
+    for i in $items; do
+        source $GH_DIR/$GH_ME/post-$X_PLATFORM-install/src/install_$i.sh
+    done
 }
 
 setup_zsh() {
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-  	source $GH_DIR/$GH_ME/post-$X_PLATFORM-install/src/install_zsh_autosuggestions.sh
-	source $GH_DIR/$GH_ME/post-$X_PLATFORM-install/src/install_zsh_syntax_highlighting.sh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    source $GH_DIR/$GH_ME/post-$X_PLATFORM-install/src/install_zsh_autosuggestions.sh
+    source $GH_DIR/$GH_ME/post-$X_PLATFORM-install/src/install_zsh_syntax_highlighting.sh
 }
 
 setup_tmux() {
-	items="tmux tmux_clipboard tmux_pkg_mngr"
-	for i in $items
-	do
-  		source $GH_DIR/$GH_ME/post-$X_PLATFORM-install/src/install_$i.sh
-	done
+    items="tmux tmux_clipboard tmux_pkg_mngr"
+    for i in $items; do
+        source $GH_DIR/$GH_ME/post-$X_PLATFORM-install/src/install_$i.sh
+    done
 }
 
 setup_vim() {
-	items="vim vim_coc vim_coc_ext vim_nerd_font"
-	for i in $items
-	do
-  		source $GH_DIR/$GH_ME/post-$X_PLATFORM-install/src/install_$i.sh
-	done
+    items="vim vim_coc vim_coc_ext vim_nerd_font"
+    for i in $items; do
+        source $GH_DIR/$GH_ME/post-$X_PLATFORM-install/src/install_$i.sh
+    done
 
 }
 
 setup_cinnamon() {
-	echo Running setup_cinnamon
+    echo Running setup_cinnamon
 }
 
 platform_check() {
-	X_PLATFORM=`source $GH_DIR/$GH_ME/post-os-install/platform_check.sh`
-	echo Detected plaform=$X_PLATFORM
+    X_PLATFORM=$(source $GH_DIR/$GH_ME/post-os-install/platform_check.sh)
+    echo Detected plaform=$X_PLATFORM
 }
 
-
 common_setup() {
-	setup_workspace_dir
-	clone_repos
-	deploy_dotfiles
-	deploy_dotfiles_pvt
-	deploy_dotdirs
-	deploy_dotdirs_pvt
+    setup_workspace_dir
+    clone_repos
+    deploy_dotfiles
+    deploy_dotfiles_pvt
+    deploy_dotdirs
+    deploy_dotdirs_pvt
 }
 
 any_platform_setup() {
-	source $GH_DIR/$GH_ME/post-$X_PLATFORM-install/src/preprocessor.sh
-	pkgs="firefox google_chrome git_delta ripgrep fzf jq"
-	for pkg in $pkgs
-	do
-		pkg_install_script=$GH_DIR/$GH_ME/post-$X_PLATFORM-install/src/install_$pkg.sh
-		if  [ ! -f "$pkg_install_script" ]; then
-			warn "$pkg_install_script does not exist for $X_PLATFORM"
-		else
-			source $pkg_install_script
-		fi
-	done
+    source $GH_DIR/$GH_ME/post-$X_PLATFORM-install/src/preprocessor.sh
+    pkgs="firefox google_chrome git_delta ripgrep fzf jq"
+    for pkg in $pkgs; do
+        pkg_install_script=$GH_DIR/$GH_ME/post-$X_PLATFORM-install/src/install_$pkg.sh
+        if [ ! -f "$pkg_install_script" ]; then
+            warn "$pkg_install_script does not exist for $X_PLATFORM"
+        else
+            source $pkg_install_script
+        fi
+    done
 }
 
 macos_only_setup() {
-	pkgs="brew mos iterm2 rectangle"
-	for pkg in $pkgs
-	do
-		source $GH_MACOS_DIR/src/install_$pkg.sh
-	done
-	setup_python
-	# setup_java
-	setup_zsh
-	setup_tmux
-	source ~/.paths.sh	
+    pkgs="brew mos iterm2 rectangle"
+    for pkg in $pkgs; do
+        source $GH_MACOS_DIR/src/install_$pkg.sh
+    done
+    setup_python
+    # setup_java
+    setup_zsh
+    setup_tmux
+    source ~/.paths.sh
 }
-
 
 platform_specific_setup() {
     echo "OS detected: $X_PLATFORM"
@@ -193,8 +178,8 @@ platform_specific_setup() {
         :
     elif [[ "$X_PLATFORM" == "macos"* ]]; then
         # Mac OSX
-	macos_setup
-	:
+        macos_setup
+        :
     elif [[ "$X_PLATFORM" == "cygwin" ]]; then
         # POSIX compatibility layer and Linux environment emulation for Windows
         :
@@ -213,10 +198,10 @@ platform_specific_setup() {
 }
 
 setup() {
-	common_setup	
-	platform_check
-	any_platform_setup
-	platform_specific_setup
+    common_setup
+    platform_check
+    any_platform_setup
+    platform_specific_setup
 }
 
 setup
